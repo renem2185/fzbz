7ce237fa2f1803d4aef2be2d7045569337a6a5a6
renem2185 <rene.murakumo@gmail.com>
2022-08-12 16:31:00 +0900

    rename var

--- post/main.go
+++ post/main.go
@@ -7,7 +7,7 @@ import (
 //	"time"
 )
 
-var wtr = bufio.NewWriter(os.Stdout)
+var writer = bufio.NewWriter(os.Stdout)
 
 const (
 	FIZZ = 3
@@ -18,34 +18,31 @@ const (
 func answerFizzBuzz(n int) string {
 	if n%FIZZ == 0 && n%BUZZ == 0 {
 		return "FizzBuzz"
-
 	} else if n%FIZZ == 0 {
 		return "Fizz"
-
 	} else if n%BUZZ == 0 {
 		return "Buzz"
-
 	} else {
 		return fmt.Sprintf("%d", n)
 	}
 }
 
 func concurrentFizzBuzz (count int) {
-	ch := make([]chan string, count)
+	recv := make([]chan string, count)
 
 	for i := 1; i <= count; i++ {
-		ch[i-1] = make(chan string)
+		recv[i-1] = make(chan string)
 
-		go func(src chan string, req int) {
+		go func(send chan string, req int) {
 			// time.Sleep(100*time.Millisecond)
 			// ↑を挟んでも100ms程度でプログラムが終了する
-			src <- answerFizzBuzz(req)
-		}(ch[i-1], i)
+			send <- answerFizzBuzz(req)
+		}(recv[i-1], i)
 	}
 	for i := 0; i < count; i++ {
-		fmt.Fprintln(wtr, <-ch[i])
+		fmt.Fprintln(writer, <-recv[i])
 	}
-	wtr.Flush()
+	writer.Flush()
 }
 
 func main() {
